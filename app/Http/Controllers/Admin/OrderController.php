7c00e13d81776658d88d41fb24e2eaad4d27cdc4
Mhysaa :3 <icrhm9x@gmail.com>
2020-03-04 23:06:19 +0700

    route + controller

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function processed()
+    {
+        //
+    }
+
+    public function edit()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+
+    Route::group(['prefix' => 'products'], function () {
+        Route::get('', 'ProductController@index');
+        Route::get('create', 'ProductController@create');
+        Route::post('', 'ProductController@store');
+        Route::get('{idProduct}/edit', 'ProductController@edit');
+        Route::put('{idProduct}', 'ProductController@update');
+        Route::delete('{idProduct}', 'ProductController@destroy');
+        Route::get('{idProduct}', 'ProductController@show');
+    });
+
+    Route::resource('users', 'UserController');
+    Route::resource('categories', 'CategoryController');
+
+    Route::get('', 'DashboardController');
+
+    Route::group(['prefix' => 'login'], function () {
+        Route::get('', 'LoginController@showLoginForm');
+        Route::post('', 'LoginController@login');
+        Route::delete('', 'LoginController@logout');
+    });
+
+    Route::group(['prefix' => 'orders'], function () {
+        Route::get('', 'OrderController@index');
+        Route::get('processed', 'OrderController@processed');
+        Route::get('{idOrders}/edit', 'OrderController@edit');
+        Route::put('{idOrders}', 'OrderController@update');
+    });
+
+
+});
